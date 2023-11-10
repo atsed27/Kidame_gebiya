b749aa67fca4197f1a88b61841749b374ef250c7
atsed27 <danielnigatu09@gmail.com>
2023-11-10 13:47:13 -0300

    tailwind is succes start

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,14 @@
+import Head from 'next/head';
+
 export default function Home() {
-  return <div>hy</div>;
+  return (
+    <div>
+      <Head>
+        <title>Kidame Gbiya</title>
+      </Head>
+      <div>
+        <h1 className="text-red-800">Kdame gebiya</h1>
+      </div>
+    </div>
+  );
 }
