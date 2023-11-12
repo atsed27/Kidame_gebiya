c5b28f995d527d91e5e33ae7e3714be2a310801d
atsed27 <danielnigatu09@gmail.com>
2023-11-12 08:32:40 -0300

    login form validation is validet

--- utils/Store.js
+++ utils/Store.js
@@ -40,6 +40,5 @@ function reducer(state, action) {
 export function StoreProvider({ children }) {
   const [state, dispatch] = useReducer(reducer, initialState);
   const value = { state, dispatch };
-  console.log(value);
   return <Store.Provider value={value}>{children}</Store.Provider>;
 }
