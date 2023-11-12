daaed9c8c2f074e577f889d5c58a0c05a8c2f56a
atsed27 <danielnigatu09@gmail.com>
2023-11-12 16:12:59 -0300

    add product to the databese

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,11 +1,14 @@
 import Users from '@/model/User';
 import db from '../../utils/db';
 import data from '@/utils/data';
+import Products from '@/model/Product';
 
 const handler = async (req, res) => {
   await db.connect();
   await Users.deleteMany();
   await Users.insertMany(data.users);
+  await Products.deleteMany();
+  await Products.insertMany(data.products);
   res.send('ok');
 };
 
