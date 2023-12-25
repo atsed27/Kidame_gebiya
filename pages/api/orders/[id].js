79fbe640985ff444914824c90493006d3fd3a2e4
atsed27 <danielnigatu09@gmail.com>
2023-12-25 17:57:52 -0300

    add order screen

--- pages/api/orders/[id].js
+++ pages/api/orders/[id].js
@@ -1,25 +1,17 @@
 import { unstable_getServerSession } from 'next-auth';
 import Nextauth from '../auth/[...nextauth]';
-import User from '@/model/User';
 import db from '@/utils/db';
 import Order from '@/model/Order';
 
 const Handler = async (req, res) => {
   const session = await unstable_getServerSession(req, res, Nextauth);
-  console.log(session);
   if (session === null) {
     return res.status(400).json('sign in requierd');
   }
   await db.connect();
-  const userFind = await User.findOne({ email: session.user.email });
-  if (!userFind) {
-    return res.status(404).json('user is not found');
-  }
-  const newOrder = new Order({
-    ...req.body,
-    user: userFind._id,
-  });
-  const order = await newOrder.save();
+
+  const order = await Order.findById(req.query.id);
+  if (!order) return res.status(404).json('order is not found');
   await db.disconnect();
   return res.status(201).json(order);
 };
