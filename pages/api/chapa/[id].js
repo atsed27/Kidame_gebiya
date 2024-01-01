4a466454b95325901d5f789e415f9ac989db19d2
atsed27 <danielnigatu09@gmail.com>
2024-01-01 09:39:04 -0300

    chapa payment is add

--- pages/api/chapa/[id].js
+++ pages/api/chapa/[id].js
@@ -10,7 +10,7 @@ const handler = async (req, res) => {
       return res.status(404).json('user is not found');
     }
     console.log(findUser.email);
-    const email = findUser.email;
+    //const email = findUser.email;
     const name1 = findUser.name;
     const option = {
       headers: {
@@ -32,7 +32,7 @@ const handler = async (req, res) => {
       phone_number: '0912345678',
       tx_ref: tx,
       callback_url: 'https://webhook.site/077164d6-29cb-40df-ba29-8a00e59a7e60',
-      return_url: 'https://www.google.com/',
+      return_url: 'https://kidame-gebiya.vercel.app/',
     };
     await axios
       .post('https://api.chapa.co/v1/transaction/initialize', data, option)
