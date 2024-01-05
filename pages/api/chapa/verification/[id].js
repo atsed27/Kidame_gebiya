1eafbfb524cdbab7f179e4ba5af0defb86063ed6
atsed27 <danielnigatu09@gmail.com>
2024-01-05 05:16:02 -0300

    verication updet to get metond from post

--- pages/api/chapa/verification/[id].js
+++ pages/api/chapa/verification/[id].js
@@ -12,7 +12,7 @@ const chapaVerification = async (req, res) => {
         },
       };
       await axios
-        .post(`https://api.chapa.co/v1/transaction/verify/${tx_ref}`, option)
+        .get(`https://api.chapa.co/v1/transaction/verify/${tx_ref}`, option)
         .then((response) => {
           console.log(response.data);
           res.send(response.data);
